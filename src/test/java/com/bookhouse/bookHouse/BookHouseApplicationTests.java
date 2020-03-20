506e4905f6f1789fe670c0495d1e38a62dde08a1
Lindsay Kelley <lmkelle3@Lindsays-MacBook-Air.local>
2020-03-20 11:07:57 -0700

    first commit

---
+++ src/test/java/com/bookhouse/bookHouse/BookHouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookhouse.bookHouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookHouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
